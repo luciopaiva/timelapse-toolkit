f1822c9ae7cc137b9517f4822862ee7ffda53306
Lucio Paiva <luciopaiva@gmail.com>
2021-06-25 01:18:08 +0100

    Fix fix-holes.js

--- fix-holes.js
+++ fix-holes.js
@@ -24,21 +24,9 @@ let nextSeq = 0;
 let seqDigits = (files.length - 1).toString().length;
 
 for (const file of files) {
-    const match = file.match(pattern);
-    const seqStr = match[1];
-    const seq = parseInt(seqStr);
-    if (seqStr.length !== seqDigits) {
-        // the sequence is fine, but we need to adjust the padding
-        const paddedNextSeq = seq.toString().padStart(seqDigits, "0");
-        const newFile = file.replace(/\d+\.png/i, `${paddedNextSeq}.png`);
-        fs.renameSync(file, newFile);
-        console.info(`Renamed "${file}" to "${newFile}".`);
-    } else if (seq !== nextSeq) {
-        // fill the hole
-        const paddedNextSeq = nextSeq.toString().padStart(seqDigits, "0");
-        const newFile = file.replace(/\d+\.png/i, `${paddedNextSeq}.png`);
-        fs.renameSync(file, newFile);
-        console.info(`Renamed "${file}" to "${newFile}".`);
-    }
+    const paddedNextSeq = nextSeq.toString().padStart(seqDigits, "0");
+    const newFile = file.replace(/\d+\.png/i, `${paddedNextSeq}.png`);
+    fs.renameSync(file, newFile);
+    console.info(`Renamed "${file}" to "${newFile}".`);
     nextSeq++;
 }
